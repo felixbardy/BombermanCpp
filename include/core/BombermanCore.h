d96988de7d8ba521f3c04a63ad8e48f5ee1d2aa1
Félix Bardy <felix@bardy.fr>
2022-05-11 16:07:19 +0200

    Added basic boundaries to grid generation

--- include/core/BombermanCore.h
+++ include/core/BombermanCore.h
@@ -17,19 +17,33 @@ namespace BombermanCore
 
     /// Masks expressing the type of a Tile
     namespace TileType {
+        /// Clears type with a bitwise and
+        const tile_mask CLEAR        = 0xfff0;
+        /// Describes a navigable tile
         const tile_mask EMPTY        = 0x1;
+        /// Describes a destructible block
         const tile_mask BLOCK        = 0x2;
-        const tile_mask UNBREACKABLE = 0x4;
+        /// Describes an unbreakable wall
+        const tile_mask UNBREAKABLE = 0x4;
+        /// [DEV] Describes the slots still available
         const tile_mask UNUSED_TYPE  = 0x8;
     };
 
     /// Masks expressing the potential content of a Tile
     namespace TileContent {
+        /// Clears content with a bitwise and
+        const tile_mask CLEAR        = 0x000f;
+        /// No content
         const tile_mask NOTHING      = 0x10;
+        /// Added max bombs bonus
         const tile_mask BONUS_BOMB   = 0x20;
+        /// Added firepower bonus
         const tile_mask BONUS_FIRE   = 0x40;
+        /// The footbomb bonus
         const tile_mask FOOTBOMB     = 0x80;
+        /// The remote bomb bonus
         const tile_mask REMOTE       = 0x100;
+        /// [DEV] Describes the slots still available
         const tile_mask UNUSED_SLOTS = 0xfe00; // All last 7 bits
     }
 } // namespace BombermanCore
--- src/core/Grid.cpp
+++ src/core/Grid.cpp
@@ -7,6 +7,14 @@ namespace BombermanCore
         this->w = width;
         this->h = height;
         this->m_grid = new Tile[h*w];
+        for (int x = 0; x < width; x++) {
+            m_grid[x].setType(TileType::UNBREAKABLE);
+            m_grid[(height-1)*width+x].setType(TileType::UNBREAKABLE);
+        }
+        for (int y = 0; y < height; y++) {
+            m_grid[y*width].setType(TileType::UNBREAKABLE);
+            m_grid[y*width + (width-1)].setType(TileType::UNBREAKABLE);
+        }
     }
     
     Grid::~Grid()
