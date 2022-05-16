cff4656e0c86ae63cb21f3646f8e2436ffc6319f
Félix Bardy <felix@bardy.fr>
2022-05-16 20:05:18 +0200

    Tidying code, working on textures, crumbling walls

--- src/BombermanApp.cpp
+++ src/BombermanApp.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 using namespace BombermanCore;
+namespace BG = BombermanGraphics;
 
 namespace BombermanGraphics
 {
@@ -15,9 +16,11 @@ namespace BombermanGraphics
           {EMPTY,               {32,  328, 12, 12}},
           {BLOCK,               {80,  328, 12, 12}},
           {WALL,                {8,   328, 12, 12}},
+
           {BOMB_1,              {58,  276, 12, 12}},
           {BOMB_2,              {35,  276, 12, 12}},
           {BOMB_3,              {11,  276, 12, 12}},
+
           {EXP_CENTER_1,        {200, 352, 12, 12}},
           {EXP_CENTER_2,        {176, 352, 12, 12}},
           {EXP_CENTER_3,        {152, 352, 12, 12}},
@@ -34,19 +37,67 @@ namespace BombermanGraphics
           {EXP_LEFT_2,          {152, 376, 12, 12}},
           {EXP_LEFT_3,          {128, 376, 12, 12}},
           {EXP_LEFT_4,          {104, 376, 12, 12}},
-          {EXP_DOWN_1,          {80 , 352, 12, 12}},
-          {EXP_DOWN_2,          {56 , 352, 12, 12}},
-          {EXP_DOWN_3,          {32 , 352, 12, 12}},
-          {EXP_DOWN_4,          {8  , 352, 12, 12}},
-          //FIXME Set the true coordinates
+          {EXP_DOWN_1,          {80 , 376, 12, 12}},
+          {EXP_DOWN_2,          {56 , 376, 12, 12}},
+          {EXP_DOWN_3,          {32 , 376, 12, 12}},
+          {EXP_DOWN_4,          {8  , 376, 12, 12}},
           {EXP_UP_1,            {296, 352, 12, 12}},
           {EXP_UP_2,            {272, 352, 12, 12}},
           {EXP_UP_3,            {248, 352, 12, 12}},
           {EXP_UP_4,            {224, 352, 12, 12}},
-          {EXP_RIGHT_1,         {200, 352, 12, 12}},
-          {EXP_RIGHT_2,         {176, 352, 12, 12}},
-          {EXP_RIGHT_3,         {152, 352, 12, 12}},
-          {EXP_RIGHT_4,         {128, 352, 12, 12}}
+          {EXP_RIGHT_1,         {392, 352, 12, 12}},
+          {EXP_RIGHT_2,         {368, 352, 12, 12}},
+          {EXP_RIGHT_3,         {344, 352, 12, 12}},
+          {EXP_RIGHT_4,         {320, 352, 12, 12}},
+
+          {CRUMBLING_BLOCK_1,   {368, 328, 12, 12}},
+          {CRUMBLING_BLOCK_2,   {392, 328, 12, 12}},
+          {CRUMBLING_BLOCK_3,   {8,   352, 12, 12}},
+          {CRUMBLING_BLOCK_4,   {32,  352, 12, 12}},
+          {CRUMBLING_BLOCK_5,   {56,  352, 12, 12}},
+          {CRUMBLING_BLOCK_6,   {80,  352, 12, 12}},
+          {CRUMBLING_BLOCK_7,   {104, 352, 12, 12}},
+
+          {PLAYER1_DOWN,        {8,   13,  16, 19}},         
+          {PLAYER1_WALK_DOWN_1, {32,  13,  16, 19}},          
+          {PLAYER1_WALK_DOWN_2, {56,  13,  14, 19}},        
+          {PLAYER1_UP,          {80,  13,  16, 19}},      
+          {PLAYER1_WALK_UP_1,   {104, 13,  16, 19}},          
+          {PLAYER1_WALK_UP_2,   {128, 13,  14, 19}},
+          {PLAYER1_SIDE,        {152, 13,  16, 19}},
+          {PLAYER1_WALK_SIDE_1, {176, 13,  16, 19}},          
+          {PLAYER1_WALK_SIDE_2, {200, 13,  14, 19}},
+
+          {PLAYER2_DOWN,        {8,   88,  16, 19}},         
+          {PLAYER2_WALK_DOWN_1, {32,  88,  16, 19}},          
+          {PLAYER2_WALK_DOWN_2, {56,  88,  14, 19}},        
+          {PLAYER2_UP,          {80,  88,  16, 19}},      
+          {PLAYER2_WALK_UP_1,   {104, 88,  16, 19}},          
+          {PLAYER2_WALK_UP_2,   {128, 88,  14, 19}},
+          {PLAYER2_SIDE,        {152, 88,  16, 19}},
+          {PLAYER2_WALK_SIDE_1, {176, 88,  16, 19}},          
+          {PLAYER2_WALK_SIDE_2, {200, 88,  14, 19}},
+
+          {PLAYER3_DOWN,        {8,   173,  16, 19}},         
+          {PLAYER3_WALK_DOWN_1, {32,  173,  16, 19}},          
+          {PLAYER3_WALK_DOWN_2, {56,  173,  14, 19}},        
+          {PLAYER3_UP,          {80,  173,  16, 19}},      
+          {PLAYER3_WALK_UP_1,   {104, 173,  16, 19}},          
+          {PLAYER3_WALK_UP_2,   {128, 173,  14, 19}},
+          {PLAYER3_SIDE,        {152, 173,  16, 19}},
+          {PLAYER3_WALK_SIDE_1, {176, 173,  16, 19}},          
+          {PLAYER3_WALK_SIDE_2, {200, 173,  14, 19}},
+
+          {PLAYER4_DOWN,        {8,   237,  16, 19}},         
+          {PLAYER4_WALK_DOWN_1, {32,  237,  16, 19}},          
+          {PLAYER4_WALK_DOWN_2, {56,  237,  14, 19}},        
+          {PLAYER4_UP,          {80,  237,  16, 19}},      
+          {PLAYER4_WALK_UP_1,   {104, 237,  16, 19}},          
+          {PLAYER4_WALK_UP_2,   {128, 237,  14, 19}},
+          {PLAYER4_SIDE,        {152, 237,  16, 19}},
+          {PLAYER4_WALK_SIDE_1, {176, 237,  16, 19}},          
+          {PLAYER4_WALK_SIDE_2, {200, 237,  14, 19}},
+
       };
 }
 
@@ -67,38 +118,6 @@ void BombermanApp::Init()
 
   // DEBUG
   // FIXME Remove test lines when not needed
-
-  m_grid.getTile(1, 1)->addBomb(
-      {
-        false,
-        4 * 60,
-        2
-      }
-  );
-
-  m_grid.getTile(7, 5)->addBomb(
-      {
-        false,
-        6 * 60,
-        2
-      }
-  );
-
-  m_grid.getTile(15, 15)->addBomb(
-    {
-      false,
-      3 * 60,
-      2
-    }
-  );
-
-  m_grid.getTile(15, 13)->addBomb(
-      {
-        false,
-        12 * 60,
-        1
-      }
-  );
 }
 
 void BombermanApp::Update()
@@ -151,7 +170,7 @@ void BombermanApp::loadTilemap()
   SDL_Surface *surface = IMG_Load("assets/sprites/tilemap.png");
   m_tilemap = SDL_CreateTextureFromSurface(renderer, surface);
 
-  m_texture_map.insert(BombermanGraphics::TEXTURE_ASSOCIATIONS);
+  m_texture_map.insert(BG::TEXTURE_ASSOCIATIONS);
 }
 
 void BombermanApp::updateTerrain()
@@ -184,11 +203,33 @@ void BombermanApp::updateTerrain()
         if (bomb->timer == 0)
           detonateBomb(x, y, bomb);
       }
+
+      // Traitement des murs qui s'écroulent
+      if (tile->getMask() & TileType::CRUMBLING_WALL) {
+        int timer = tile->getCrumblingTimer();
+        tile->setCrumblingTimer(--timer);
+        if (timer == 0) tile->setType(TileType::EMPTY);
+      }
     }
 }
 
 void BombermanApp::handleInput()
 {
+  int unit_size = getWidth() / m_grid.getWidth();
+  int mx,my,tx,ty;
+  SDL_GetMouseState(&mx,&my);
+
+  SDL_Event e;
+  while(SDL_PollEvent(&e)) {
+    if (e.type == SDL_MOUSEBUTTONDOWN) {
+      Uint8 pressed = e.button.button;
+      if (pressed == SDL_BUTTON_LEFT) {
+        tx = mx / unit_size;
+        ty = my / unit_size;
+        m_grid.getTile(tx,ty)->addBomb({false,BOMB_DEFAUT_TIMER,2});
+      }
+    }
+  }
 }
 
 void BombermanApp::detonateBomb(int x, int y, bomb_data *bomb)
@@ -214,8 +255,8 @@ void BombermanApp::propagateExplosion(int x, int y, Direction dir, int power) {
   if (power == 0) return;
 
   Tile* tile = nullptr;
-  bool keep_going = true;
-  while(keep_going) {
+
+  while(power > 0) {
     switch(dir) {
       case NORTH:
         tile = m_grid.getTile(x,--y);
@@ -288,14 +329,17 @@ void BombermanApp::propagateExplosion(int x, int y, Direction dir, int power) {
           6 * EXPLOSION_STEP_DURATION
         }
       );
+      // Et on décrémente la puissance
+      power--;
 
 
     } // FIN SI (la case est vide)
 
     // Si la case n'est pas vide:
     else if (tile->getMask() & TileType::BLOCK) {
-        //TODO Lancer la destruction du bloc
-
+        // Lancer la destruction du bloc
+        tile->setType(TileType::CRUMBLING_WALL);
+        tile->setCrumblingTimer(7 * CRUMBLING_STEP_DURATION);
         // Une fois la destruction lancée, arrêter l'explosion
         return;
     }
@@ -313,18 +357,18 @@ SDL_Rect BombermanApp::getCurrentTexture(int x, int y)
 
   if (tile->getMask() & TileContent::BOMB)
   {
-    switch ((4 * getFramecount() / getFramerate()) % 4)
+    switch ((BG::BOMB_LOOP_SPEED * getFramecount() / getFramerate()) % 4)
     {
     case 0:
-      return m_texture_map[BombermanGraphics::BOMB_1];
+      return m_texture_map[BG::BOMB_1];
     case 1:
-      return m_texture_map[BombermanGraphics::BOMB_2];
+      return m_texture_map[BG::BOMB_2];
     case 2:
-      return m_texture_map[BombermanGraphics::BOMB_3];
+      return m_texture_map[BG::BOMB_3];
     case 3:
-      return m_texture_map[BombermanGraphics::BOMB_2];
+      return m_texture_map[BG::BOMB_2];
     default:
-      return m_texture_map[BombermanGraphics::MISSING_TEXTURE];
+      return m_texture_map[BG::MISSING_TEXTURE];
     }
   }
 
@@ -336,73 +380,73 @@ SDL_Rect BombermanApp::getCurrentTexture(int x, int y)
       switch (tile->getExplosion()->dir)
       {
       case NONE:
-        return m_texture_map[BombermanGraphics::EXP_CENTER_4];
+        return m_texture_map[BG::EXP_CENTER_4];
       case HORIZONTAL:
-        return m_texture_map[BombermanGraphics::EXP_HORIZONTAL_4];
+        return m_texture_map[BG::EXP_HORIZONTAL_4];
       case VERTICAL:
-        return m_texture_map[BombermanGraphics::EXP_VERTICAL_4];
+        return m_texture_map[BG::EXP_VERTICAL_4];
       case SOUTH:
-        return m_texture_map[BombermanGraphics::EXP_DOWN_4];
+        return m_texture_map[BG::EXP_DOWN_4];
       case NORTH:
-        return m_texture_map[BombermanGraphics::EXP_UP_4];
+        return m_texture_map[BG::EXP_UP_4];
       case EAST:
-        return m_texture_map[BombermanGraphics::EXP_RIGHT_4];
+        return m_texture_map[BG::EXP_RIGHT_4];
       case WEST:
-        return m_texture_map[BombermanGraphics::EXP_LEFT_4];
+        return m_texture_map[BG::EXP_LEFT_4];
       }
     case 1:
       switch (tile->getExplosion()->dir)
       {
       case NONE:
-        return m_texture_map[BombermanGraphics::EXP_CENTER_3];
+        return m_texture_map[BG::EXP_CENTER_3];
       case HORIZONTAL:
-        return m_texture_map[BombermanGraphics::EXP_HORIZONTAL_3];
+        return m_texture_map[BG::EXP_HORIZONTAL_3];
       case VERTICAL:
-        return m_texture_map[BombermanGraphics::EXP_VERTICAL_3];
+        return m_texture_map[BG::EXP_VERTICAL_3];
       case SOUTH:
-        return m_texture_map[BombermanGraphics::EXP_DOWN_3];
+        return m_texture_map[BG::EXP_DOWN_3];
       case NORTH:
-        return m_texture_map[BombermanGraphics::EXP_UP_3];
+        return m_texture_map[BG::EXP_UP_3];
       case EAST:
-        return m_texture_map[BombermanGraphics::EXP_RIGHT_3];
+        return m_texture_map[BG::EXP_RIGHT_3];
       case WEST:
-        return m_texture_map[BombermanGraphics::EXP_LEFT_3];
+        return m_texture_map[BG::EXP_LEFT_3];
       }
     case 2:
       switch (tile->getExplosion()->dir)
       {
       case NONE:
-        return m_texture_map[BombermanGraphics::EXP_CENTER_2];
+        return m_texture_map[BG::EXP_CENTER_2];
       case HORIZONTAL:
-        return m_texture_map[BombermanGraphics::EXP_HORIZONTAL_2];
+        return m_texture_map[BG::EXP_HORIZONTAL_2];
       case VERTICAL:
-        return m_texture_map[BombermanGraphics::EXP_VERTICAL_2];
+        return m_texture_map[BG::EXP_VERTICAL_2];
       case SOUTH:
-        return m_texture_map[BombermanGraphics::EXP_DOWN_2];
+        return m_texture_map[BG::EXP_DOWN_2];
       case NORTH:
-        return m_texture_map[BombermanGraphics::EXP_UP_2];
+        return m_texture_map[BG::EXP_UP_2];
       case EAST:
-        return m_texture_map[BombermanGraphics::EXP_RIGHT_2];
+        return m_texture_map[BG::EXP_RIGHT_2];
       case WEST:
-        return m_texture_map[BombermanGraphics::EXP_LEFT_2];
+        return m_texture_map[BG::EXP_LEFT_2];
       }
     case 3:
       switch (tile->getExplosion()->dir)
       {
       case NONE:
-        return m_texture_map[BombermanGraphics::EXP_CENTER_1];
+        return m_texture_map[BG::EXP_CENTER_1];
       case HORIZONTAL:
-        return m_texture_map[BombermanGraphics::EXP_HORIZONTAL_1];
+        return m_texture_map[BG::EXP_HORIZONTAL_1];
       case VERTICAL:
-        return m_texture_map[BombermanGraphics::EXP_VERTICAL_1];
+        return m_texture_map[BG::EXP_VERTICAL_1];
       case SOUTH:
-        return m_texture_map[BombermanGraphics::EXP_DOWN_1];
+        return m_texture_map[BG::EXP_DOWN_1];
       case NORTH:
-        return m_texture_map[BombermanGraphics::EXP_UP_1];
+        return m_texture_map[BG::EXP_UP_1];
       case EAST:
-        return m_texture_map[BombermanGraphics::EXP_RIGHT_1];
+        return m_texture_map[BG::EXP_RIGHT_1];
       case WEST:
-        return m_texture_map[BombermanGraphics::EXP_LEFT_1];
+        return m_texture_map[BG::EXP_LEFT_1];
       }
     default:
       std::cerr << "Something seems off with explosion at " << x << "," << y << std::endl;
@@ -410,11 +454,32 @@ SDL_Rect BombermanApp::getCurrentTexture(int x, int y)
   }
 
   if (tile->getMask() & TileType::BLOCK)
-    return m_texture_map[BombermanGraphics::BLOCK];
+    return m_texture_map[BG::BLOCK];
   if (tile->getMask() & TileType::UNBREAKABLE)
-    return m_texture_map[BombermanGraphics::WALL];
-  if (tile->getMask() & TileType::CLEAR)
-    return m_texture_map[BombermanGraphics::EMPTY];
+    return m_texture_map[BG::WALL];
+  if (tile->getMask() & TileType::EMPTY)
+    return m_texture_map[BG::EMPTY];
+  if (tile->getMask() & TileType::CRUMBLING_WALL) {
+    switch(tile->getCrumblingTimer()/CRUMBLING_STEP_DURATION) {
+      case 6:
+        return m_texture_map[BG::CRUMBLING_BLOCK_1];
+      case 5:
+        return m_texture_map[BG::CRUMBLING_BLOCK_2];
+      case 4:
+        return m_texture_map[BG::CRUMBLING_BLOCK_3];
+      case 3:
+        return m_texture_map[BG::CRUMBLING_BLOCK_4];
+      case 2:
+        return m_texture_map[BG::CRUMBLING_BLOCK_5];
+      case 1:
+        return m_texture_map[BG::CRUMBLING_BLOCK_6];
+      case 0:
+        return m_texture_map[BG::CRUMBLING_BLOCK_7];
+      default:
+        return m_texture_map[BG::MISSING_TEXTURE];
+    }
+  }
+    
 
-  return m_texture_map[BombermanGraphics::MISSING_TEXTURE];
+  return m_texture_map[BG::MISSING_TEXTURE];
 }
